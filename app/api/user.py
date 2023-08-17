e16d2fbd9ac9900a86c1db945dc4b2fceba6b73a
tangyx <851565913@qq.com>
2023-08-17 16:02:29 +0800

    新增用户接口

--- app/api/user.py
+++ app/api/user.py
@@ -22,9 +22,9 @@ class UserInfo(BaseModel):
 
 class UserParam(BaseModel):
     id: str = Field(None)
-    username: str
-    password: str
-    name: str
+    user_name: str = Field(None)
+    password: str = Field(None)
+    name: str = Field(None)
 
 
 @router.post("/login", summary="用户登录认证")
@@ -60,27 +60,54 @@ async def get_user_info(tokenInfo: Union[str, Any] = Depends(check_token), db: S
     return resp.ok(data=data)
 
 
-@router.get("/user/add")
+@router.get("/user/list")
+async def list(pageSize: int, current: int, db: Session = Depends(getSesion)):
+    offset = (current - 1) * pageSize
+    data = db.query(User_Info).order_by(User_Info.create_time.desc()).offset(offset).limit(pageSize).all()
+    total = db.query(User_Info).order_by(User_Info.create_time.desc()).offset(offset).limit(pageSize).count()
+    for user in data:
+        user.password = ""
+    return {"total": total, "list": data}
+
+
+@router.post("/user/add")
 async def add_user(user_info: UserParam, tokenInfo: Union[str, Any] = Depends(check_token), db: Session = Depends(getSesion)):
-    db_user = db.query(User_Info).filter(User_Info.name == user_info.username).one_or_none()
+    db_user = db.query(User_Info).filter(User_Info.user_name == user_info.user_name).one_or_none()
     if db_user:
         return resp.fail2(message="该用户名已经存在")
-    user = User_Info(user_name=user_info.username, password=user_info.password)
+    user = User_Info(user_name=user_info.user_name, password=user_info.password)
     user.id = get_id()
     user.name = user_info.name
     user.create_time = datetime.now()
     db.add(user)
+    db.commit()
     return {"message": "ok"}
 
 
-@router.get("/user/update")
+@router.post("/user/update")
 async def update_user(user_info: UserParam, tokenInfo: Union[str, Any] = Depends(check_token), db: Session = Depends(getSesion)):
     db_user = db.query(User_Info).filter(User_Info.id == user_info.id).one_or_none()
     if not db_user:
         return resp.fail2(message="用户不存在")
     db_user.name = user_info.name
-    db_user.password = user_info.password
-    db_user.user_name = user_info.username
+    db_user.user_name = user_info.user_name
     db_user.update_time = datetime.now()
+    db.commit()
+    return {"message": "ok"}
+
+
+@router.post("/user/del")
+async def del_user(user_info: UserParam, tokenInfo: Union[str, Any] = Depends(check_token), db: Session = Depends(getSesion)):
+    if user_info.id == "1":
+        return resp.fail2(message="管理员不允许删除")
+    db.query(User_Info).filter(User_Info.id == user_info.id).delete()
+    db.commit()
+    return {"message": "ok"}
+
+
+@router.post("/user/password")
+async def del_user(user_info: UserParam, tokenInfo: Union[str, Any] = Depends(check_token), db: Session = Depends(getSesion)):
+    db_user = db.query(User_Info).filter(User_Info.id == user_info.id).one()
+    db_user.password = user_info.password
     db.commit()
     return {"message": "ok"}
